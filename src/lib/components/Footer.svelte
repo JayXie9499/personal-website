1878db5835895c5d91a6c5b099df331a12f948be
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-02-24 14:38:29 +0800

    feat: add icon credits link in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,8 +1,14 @@
 <!-- Footer -->
 <footer class="mt-auto border-t border-border bg-bg-primary px-6 py-12" data-testid="footer">
-	<div class="mx-auto w-full max-w-max-width px-6">
+	<div class="mx-auto flex w-full max-w-max-width justify-between px-6">
 		<p class="text-left font-(family-name:--font-family-mono) text-xs text-text-muted">
 			Copyright © {new Date().getFullYear()} Meng Che Hsieh. All rights reserved.
+		</p>
+
+		<p
+			class="text-right font-(family-name:--font-family-mono) text-xs text-text-muted underline-offset-3 hover:underline"
+		>
+			<a href="/credits">Icon Credits</a>
 		</p>
 	</div>
 </footer>
