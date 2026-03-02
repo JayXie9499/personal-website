2c26d25127f60e2ee8b6885ed9ff955fde1486f5
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-03-02 16:22:18 +0800

    feat(404-page): update layout and styling for 404 error page

--- src/routes/[...404]/+page.svelte
+++ src/routes/[...404]/+page.svelte
@@ -1,14 +1,10 @@
-<div class="px-6 text-center">
-	<h1 class="text-9xl font-bold text-gray-200">404</h1>
-
-	<div class="pointer-events-none absolute inset-0 flex items-center justify-center">
-		<h2 class="text-3xl font-semibold text-gray-800">頁面不存在</h2>
-	</div>
-
+<div class="flex h-dvh w-dvw flex-col items-center justify-center">
+	<h1 class="text-8xl font-bold text-gray-200">404</h1>
+	<p class="text-3xl text-text-secondary">頁面不存在</p>
 	<div class="mt-8 flex flex-col justify-center gap-4 sm:flex-row">
 		<a
 			href="/"
-			class="rounded-lg bg-indigo-600 px-6 py-3 font-medium text-white transition hover:bg-indigo-700"
+			class="rounded-lg bg-indigo-600 px-6 py-3 text-lg font-medium text-white transition hover:bg-indigo-700"
 		>
 			返回
 		</a>
